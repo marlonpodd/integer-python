59e61f04be4e2721d985f0db780660bec4db555a
Marlon Poddalgoda <47573256+marlonpodd@users.noreply.github.com>
2019-04-03 20:26:04 -0400

    Add files via upload

---
+++ integer.py
@@ -0,0 +1,12 @@
+import math
+
+integer = int(input("Type a number: "))
+
+if integer > 0:
+	print("Positive integer")
+	print()
+elif integer < 0:
+	print("Negative integer")
+	print()
+else:
+	print("Neutral or 0")
